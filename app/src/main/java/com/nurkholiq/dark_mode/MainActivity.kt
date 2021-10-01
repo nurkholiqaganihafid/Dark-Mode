7200ed4ad445a3486f467997c2dd9d4fea026cda
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-01 16:03:09 +0700

    Menambahkan NewActivity.kt dan modified MainActivity.kt

--- app/src/main/java/com/nurkholiq/dark_mode/MainActivity.kt
+++ app/src/main/java/com/nurkholiq/dark_mode/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.nurkholiq.dark_mode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Dark Mode"
+
+
+        val switchMaterial = findViewById<SwitchMaterial>(R.id.switch_material)
+        val buttonSample = findViewById<Button>(R.id.btn_sample)
+
+
+        buttonSample.setOnClickListener {
+            startActivity(Intent(this,NewActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/nurkholiq/dark_mode/NewActivity.kt
@@ -0,0 +1,14 @@
+package com.nurkholiq.dark_mode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+
+        supportActionBar?.title = "New Activity"
+
+    }
+}
